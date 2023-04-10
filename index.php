13279f623b244031918c215498463b4b35170506
John Larimore <john.m.larimore@gmail.com>
2023-04-09 22:02:19 -0600

    Added index

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+?>
+<html>
+	<head>
+		<title>PHP & MySQL</title>
+	</head>
+	<body>
+		<ul>
+			<li><a href="ch2/">CH2</a></li>
+			<li><a href="ch3/">CH3</a></li>
+			<li><a href="ch4/">CH4</a></li>
+			<li><a href="ch5/">CH5</a></li>
+			<li><a href="ch6/">CH6</a></li>
+			<li><a href="ch7/">CH7</a></li>
+			<li><a href="ch8/">CH8</a></li>
+			<li><a href="ch9/">CH9</a></li>
+			<li><a href="ch10/">CH10</a></li>
+			<li><a href="ch11/">CH11</a></li>
+			<li><a href="ch12/">CH12</a></li>
+			<li><a href="ch13/">CH13</a></li>
+			<li><a href="ch14/">CH14</a></li>
+			<li><a href="ch15/">CH15</a></li>
+			<li><a href="ch16/">CH16</a></li>
+			<li><a href="ch17/">CH17</a></li>
+			<li><a href="ch18/">CH18</a></li>
+			<li><a href="ch19/">CH19</a></li>
+			<li><a href="ch20/">CH20</a></li>
+			<li><a href="ch21/">CH21</a></li>
+			<li><a href="ch22/">CH22</a></li>
+			<li><a href="ch23/">CH23</a></li>
+			<li><a href="ch24/">CH24</a></li>
+		</ul>
+	</body>
+</html>
